905c10e1c650ab80e4283fdc3757ac76c716119b
Vinuka Aksika <vinukawi10@gmail.com>
2021-06-25 19:54:39 +0530

    q9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -163,20 +163,20 @@ public class Main {
          short [] array={1,2,3,4,5,6};
          System.out.println(array); // reference will be displayed as the output //
          System.out.println(Arrays.toString(array));
-         **/
+       **/
 
-        //q9
+      //q9
 
-        int[]numbers={1,2,3,4,5,6,7,8,9,10};
-        for (int i=0;i<numbers.length;i++){
-            for (int j=0;i>=j;j++){
-                if (j!=i){
-                    if (numbers[i]+numbers[j]==6){
-                        System.out.println("{"+numbers[i]+","+numbers[j]+"}");
-                    }
+      /**int[]numbers={1,2,3,4,5,6,7,8,9,10};
+      for (int i=0;i<numbers.length;i++){
+          for (int j=0;i>=j;j++){
+              if (j!=i){
+                  if (numbers[i]+numbers[j]==6){
+                      System.out.println("{"+numbers[i]+","+numbers[j]+"}");
+                  }
                 }
             }
-        }
+        }**/
 
 
 
